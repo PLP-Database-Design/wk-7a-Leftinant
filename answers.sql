b41631ce0aab74a177221cbc30c3ede9bd2963a4
Benjamin <157468010+Leftinant@users.noreply.github.com>
2025-05-05 08:25:23 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,11 +1,11 @@
 Question One
-CREATE TABLE ProductDetail (
+CREATE TABLE ProductDetail_1NF (
     OrderID INT,
     CustomerName VARCHAR(255),
     Product VARCHAR(255)
 );
 
-INSERT INTO ProductDetail (OrderID, CustomerName, Product)
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
 SELECT
     OrderID,
     CustomerName,
@@ -14,3 +14,26 @@ FROM
     ProductDetail
 CROSS APPLY
     STRING_SPLIT(Products, ',');
+
+Question Two
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(255)
+);
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+CREATE TABLE OrderDetail_2NF (
+    OrderID INT,
+    Product VARCHAR(255),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetail_2NF (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
+
+
